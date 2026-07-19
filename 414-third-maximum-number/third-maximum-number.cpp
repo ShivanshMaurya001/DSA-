0f8c21e7b7ee01ad53bf6d7e24e417a5ff786c19
Shivansh Maurya <shivanshindia001@gmail.com>
2026-07-19 23:34:24 +0530

    Time: 4 ms (18.58%) | Memory: 13 MB (47.70%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
 
-        // Step 1: Sort
         sort(nums.begin(), nums.end());
 
-        // Step 2: Reverse (Descending)
         reverse(nums.begin(), nums.end());
 
         int n = nums.size();
