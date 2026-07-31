4fe863cb02e2fc5701a3e9f9553fe799b0a23440
Shivansh Maurya <shivanshindia001@gmail.com>
2026-07-31 23:27:17 +0530

    Time: 2 ms (22.35%) | Memory: 9.4 MB (42.72%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+
+        unordered_map<char, int> mp;
+
+        for(int i = 0; i < s.size(); i++){
+            mp[s[i]]++;
+        }
+
+        int ans = 0;
+        bool found = false;
+
+        for(auto x : mp){
+            int freq = x.second;
+
+            if(freq % 2 == 0){
+                ans += freq;
+            }
+            else{
+                ans += freq - 1; 
+                found = true;   
+            }
+        }
+
+        if(found)
+        {
+            ans++;
+        }
+
+        return ans;
+    }
+};     
